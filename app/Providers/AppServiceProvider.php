74761d8bfcbbde3dac09043c6ba1e28e2b27a0be
Mikhael Bagus <mikhael@perqara.com>
2023-06-19 22:44:48 +0700

    ps

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,41 +3,44 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Services\Logout\LogoutService;
-use App\Services\Logout\LogoutServiceContract;
-use App\Services\Register\RegisterService;
-use App\Services\Register\RegisterServiceContract;
-use App\Services\Profile\ProfileService;
-use App\Services\Profile\ProfileServiceContract;
-use App\Services\ForgotPassword\ForgotPasswordService;
-use App\Services\ForgotPassword\ForgotPasswordServiceContract;
-
 use App\Services\AboutUs\AboutUsService;
 use App\Services\AboutUs\AboutUsServiceContract;
 use App\Services\ContactUs\ContactUsService;
 use App\Services\ContactUs\ContactUsServiceContract;
 use App\Services\Disclaimer\DisclaimerService;
 use App\Services\Disclaimer\DisclaimerServiceContract;
-use App\Services\Faq\FaqService;
-use App\Services\Faq\FaqServiceContract;
-use App\Services\Media\MediaService;
-use App\Services\Media\MediaServiceContract;
-use App\Services\News\NewsService;
-use App\Services\News\NewsServiceContract;
 use App\Services\PrivacyPolicy\PrivacyPolicyService;
 use App\Services\PrivacyPolicy\PrivacyPolicyServiceContract;
 use App\Services\TermCondition\TermConditionService;
 use App\Services\TermCondition\TermConditionServiceContract;
-
-use App\Services\Ingredient\IngredientService;
-use App\Services\Ingredient\IngredientServiceContract;
-use App\Services\Product\ProductService;
-use App\Services\Product\ProductServiceContract;
-use App\Services\Transaction\TransactionService;
-use App\Services\Transaction\TransactionServiceContract;
-
-use Illuminate\Support\Collection;
-use Illuminate\Pagination\LengthAwarePaginator;
+use App\Services\Logout\LogoutService;
+use App\Services\Logout\LogoutServiceContract;
+use App\Services\ForgotPassword\ForgotPasswordService;
+use App\Services\ForgotPassword\ForgotPasswordServiceContract;
+use App\Services\Register\RegisterService;
+use App\Services\Register\RegisterServiceContract;
+use App\Services\Profile\ProfileService;
+use App\Services\Profile\ProfileServiceContract;
+use App\Services\News\NewsService;
+use App\Services\News\NewsServiceContract;
+use App\Services\Faq\FaqService;
+use App\Services\Faq\FaqServiceContract;
+use App\Services\Constraction\ConstractionService;
+use App\Services\Constraction\ConstractionServiceContract;
+use App\Services\Type\TypeService;
+use App\Services\Type\TypeServiceContract;
+use App\Services\Unit\UnitService;
+use App\Services\Unit\UnitServiceContract;
+use App\Services\Feeder\FeederService;
+use App\Services\Feeder\FeederServiceContract;
+use App\Services\Asset\AssetService;
+use App\Services\Asset\AssetServiceContract;
+use App\Services\AssetToAsset\AssetToAssetService;
+use App\Services\AssetToAsset\AssetToAssetServiceContract;
+use App\Services\Network\NetworkService;
+use App\Services\Network\NetworkServiceContract;
+use App\Services\Gallery\GalleryService;
+use App\Services\Gallery\GalleryServiceContract;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -49,26 +52,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->bind(
-            LogoutServiceContract::class,
-            LogoutService::class
-        );
-
-        $this->app->bind(
-            RegisterServiceContract::class,
-            RegisterService::class
-        );
-
-        $this->app->bind(
-            ProfileServiceContract::class,
-            ProfileService::class
-        );
-
-        $this->app->bind(
-            ForgotPasswordServiceContract::class,
-            ForgotPasswordService::class
-        );
-
         $this->app->bind(
             AboutUsServiceContract::class,
             AboutUsService::class
@@ -85,21 +68,6 @@ class AppServiceProvider extends ServiceProvider
         );
 
         $this->app->bind(
-            FaqServiceContract::class,
-            FaqService::class
-        );
-
-        $this->app->bind(
-            MediaServiceContract::class,
-            MediaService::class
-        );
-
-        $this->app->bind(
-            NewsServiceContract::class,
-            NewsService::class
-        );
-
-        $this->app->bind(
             PrivacyPolicyServiceContract::class,
             PrivacyPolicyService::class
         );
@@ -110,18 +78,68 @@ class AppServiceProvider extends ServiceProvider
         );
 
         $this->app->bind(
-            IngredientServiceContract::class,
-            IngredientService::class
+            LogoutServiceContract::class,
+            LogoutService::class
         );
 
         $this->app->bind(
-            ProductServiceContract::class,
-            ProductService::class
+            RegisterServiceContract::class,
+            RegisterService::class
         );
 
         $this->app->bind(
-            TransactionServiceContract::class,
-            TransactionService::class
+            ForgotPasswordServiceContract::class,
+            ForgotPasswordService::class
+        );
+
+        $this->app->bind(
+            ProfileServiceContract::class,
+            ProfileService::class
+        );
+
+        $this->app->bind(
+            NewsServiceContract::class,
+            NewsService::class
+        );
+
+        $this->app->bind(
+            ConstractionServiceContract::class,
+            ConstractionService::class
+        );
+
+        $this->app->bind(
+            TypeServiceContract::class,
+            TypeService::class
+        );
+
+        $this->app->bind(
+            UnitServiceContract::class,
+            UnitService::class
+        );
+
+        $this->app->bind(
+            FeederServiceContract::class,
+            FeederService::class
+        );
+
+        $this->app->bind(
+            AssetServiceContract::class,
+            AssetService::class
+        );
+
+        $this->app->bind(
+            AssetToAssetServiceContract::class,
+            AssetToAssetService::class
+        );
+
+        $this->app->bind(
+            NetworkServiceContract::class,
+            NetworkService::class
+        );
+
+        $this->app->bind(
+            GalleryServiceContract::class,
+            GalleryService::class
         );
     }
 
@@ -133,18 +151,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
-            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
-            return new LengthAwarePaginator(
-                $this->forPage($page, $perPage),
-                $total ?: $this->count(),
-                $perPage,
-                $page,
-                [
-                    'path' => LengthAwarePaginator::resolveCurrentPath(),
-                    'pageName' => $pageName,
-                ]
-            );
-        });
     }
 }
