d9dfe226fb5d37491bda8cf0fafab4de52384ea8
Mikhael Bagus <mikhael@perqara.com>
2023-06-19 22:43:20 +0700

    ps

--- config/cartalyst.sentinel.php
+++ config/cartalyst.sentinel.php
@@ -11,10 +11,10 @@
  * bundled with this package in the LICENSE file.
  *
  * @package    Sentinel
- * @version    4.0.0
+ * @version    6.0.0
  * @author     Cartalyst LLC
  * @license    BSD License (3-clause)
- * @copyright  (c) 2011-2020, Cartalyst LLC
+ * @copyright  (c) 2011-2022, Cartalyst LLC
  * @link       https://cartalyst.com
  */
 
@@ -123,7 +123,7 @@ return [
     */
 
     'checkpoints' => [
-        // 'throttle',
+        'throttle',
         'activation',
     ],
 
