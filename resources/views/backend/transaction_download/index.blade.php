67edc5d2868da9a93e4094bbce2249c490d4faaa
MikhaelBagus24 <mikhael@perqara.com>
2023-09-19 11:33:05 +0700

    Update index.blade.php

--- resources/views/backend/transaction_download/index.blade.php
+++ resources/views/backend/transaction_download/index.blade.php
@@ -185,11 +185,10 @@
 
                 <div class="panel-footer">
                     <input type="hidden" value="{{old('previousUrl') ? old('previousUrl') : url()->previous()}}" name="previousUrl">
-                    <a href="{{old('previousUrl') ? old('previousUrl') : url()->previous()}}" class="btn btn-flat btn-default btn-sm"><i class="fa fa-reply"></i> @lang('auth.form_user_cancel_btn')</a>
                     
                     <div class="pull-right">
                         <button type="submit" class="btn ladda-button btn-success btn-sm" data-style="zoom-in">
-                            <span class="ladda-label"><i class="fa fa-save"></i> @lang('auth.form_user_submit_btn')</span>
+                            <span class="ladda-label"><i class="fa fa-save"></i> Download</span>
                             <span class="ladda-spinner"><div class="ladda-progress" style="width: 0px;"></div></span></button>
                     </div>
                     
